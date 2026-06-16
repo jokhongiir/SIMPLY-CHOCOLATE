2d251e6286a5e877e9947484eef78b6877db9ff2
raxmatjonoww <raxmatjonovjahongir5@gmail.com>
2026-06-16 18:00:23 +0500

    Taste component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from "./components/header/Header"
 import "./index.css"
 import Home from './components/home/Home'
+import Taste from './components/taste/Taste'
 
 const App = () => {
   return (
     <div className='App'>
       <Header/>
       <Home/>
+      <Taste/>
     </div>
   )
 }
