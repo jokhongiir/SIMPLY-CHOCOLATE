af3f0d319ecb2b7be8af9ee814a7fdd8fbf8a786
raxmatjonoww <raxmatjonovjahongir5@gmail.com>
2026-06-16 17:05:23 +0500

    Chocolate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "./components/header/Header"
 import "./index.css"
+import Home from './components/home/Home'
 
 const App = () => {
   return (
     <div className='App'>
       <Header/>
+      <Home/>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section id="home">
+      <main className="hero">
+        <div className="overlay"></div>
 
-export default Home
+        <div className="hero-content">
+          <h1>
+            Treat yourself or a loved one to our finest ingredients for a moment
+            of pure delight!
+          </h1>
+
+          <div className="hero-buttons">
+            <button className="buy-btn">Buy now</button>
+            <button className="made-btn">How it's made</button>
+          </div>
+        </div>
+
+        <div className="scroll-down">
+          <span>scroll down</span>
+          <div className="circle">
+            <span>↓</span>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Home;
