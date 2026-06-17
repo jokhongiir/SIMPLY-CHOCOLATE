ff5a4a556ce30226e8794736059f938dc8937663
raxmatjonoww <raxmatjonovjahongir5@gmail.com>
2026-06-17 14:40:54 +0500

    Choco

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,76 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Products.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+// import "swiper/css/pagination";
+
 const PRODUCTS_DATA = [
-  {
-    id: 1,
-    name: "ORANGE",
-    type: "Dark chocolate",
-    price: "45 UAH",
-    img: "/images/orange.png",
-  },
-  {
-    id: 2,
-    name: "APPLE & CRANBERRY",
-    type: "Milk chocolate",
-    price: "50 UAH",
-    img: "/images/apple.png",
-  },
-  {
-    id: 3,
-    name: "LIME & SEA SALT",
-    type: "Dark chocolate",
-    price: "66 UAH",
-    img: "/images/lime.png",
-  },
-  {
-    id: 4,
-    name: "PINEAPPLE",
-    type: "Dark chocolate",
-    price: "54 UAH",
-    img: "/images/pineapple.png",
-  },
-  {
-    id: 5,
-    name: "CLASSIC",
-    type: "Milk chocolate",
-    price: "45 UAH",
-    img: "/images/classic_milk.png",
-  },
-  {
-    id: 6,
-    name: "HONEY",
-    type: "Milk chocolate",
-    price: "50 UAH",
-    img: "/images/honey.png",
-  },
-  {
-    id: 7,
-    name: "ROASTED FRUITS",
-    type: "Dark chocolate",
-    price: "66 UAH",
-    img: "/images/roasted.png",
-  },
-  {
-    id: 8,
-    name: "CLASSIC",
-    type: "White chocolate",
-    price: "54 UAH",
-    img: "/images/classic_white.png",
-  },
+  { id: 1, name: "ORANGE", type: "Dark chocolate", price: "45 UAH", img: "/images/orange.png" },
+  { id: 2, name: "APPLE & CRANBERRY", type: "Milk chocolate", price: "50 UAH", img: "/images/apple.png" },
+  { id: 3, name: "LIME & SEA SALT", type: "Dark chocolate", price: "66 UAH", img: "/images/lime.png" },
+  { id: 4, name: "PINEAPPLE", type: "Dark chocolate", price: "54 UAH", img: "/images/pineapple.png" },
+  { id: 5, name: "CLASSIC", type: "Milk chocolate", price: "45 UAH", img: "/images/classic_milk.png" },
+  { id: 6, name: "HONEY", type: "Milk chocolate", price: "50 UAH", img: "/images/honey.png" },
+  { id: 7, name: "ROASTED FRUITS", type: "Dark chocolate", price: "66 UAH", img: "/images/roasted.png" },
+  { id: 8, name: "CLASSIC", type: "White chocolate", price: "54 UAH", img: "/images/classic_white.png" },
 ];
 
 export default function Products() {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const itemsPerPage = 4;
-  const totalPages = Math.ceil(PRODUCTS_DATA.length / itemsPerPage);
-
-  const visibleProducts = PRODUCTS_DATA.slice(
-    currentPage * itemsPerPage,
-    currentPage * itemsPerPage + itemsPerPage
-  );
-
   const handleMove = (e) => {
     const card = e.currentTarget;
     const rect = card.getBoundingClientRect();
@@ -96,47 +44,54 @@ export default function Products() {
 
   return (
     <section className="products-section">
-      <h2 className="products-title">
-        OUR <span>PRODUCTS</span>
-      </h2>
+      <div className="products-wrapper">
+        <h2 className="products-title">
+          OUR <span>PRODUCTS</span>
+        </h2>
 
-      <div className="products-grid">
-        {visibleProducts.map((product) => (
-          <article
-            key={product.id}
-            className="product-card"
-            onMouseMove={handleMove}
-            onMouseLeave={handleLeave}
-          >
-            <div className="product-image-box">
-              <img
-                src={product.img}
-                alt={product.name}
-                className="product-image"
-              />
-            </div>
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          spaceBetween={30}
+          loop={true}
+          grabCursor={true}
+          speed={3000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            0: { slidesPerView: 1 },
+            576: { slidesPerView: 2 },
+            992: { slidesPerView: 3 },
+            1200: { slidesPerView: 4 },
+          }}
+        >
+          {PRODUCTS_DATA.map((product) => (
+            <SwiperSlide className="swipper" key={product.id}>
+              <div
+                className="product-card"
+                onMouseMove={handleMove}
+                onMouseLeave={handleLeave}
+              >
+                <div className="product-image-box">
+                  <img
+                    src={product.img}
+                    alt={product.name}
+                    className="product-image"
+                  />
+                </div>
 
-            <h3 className="product-name">{product.name}</h3>
+                <h3 className="product-name">{product.name}</h3>
+                <p className="product-type">{product.type}</p>
 
-            <p className="product-type">{product.type}</p>
-
-            <button className="product-price">
-              {product.price}
-            </button>
-          </article>
-        ))}
-      </div>
-
-      <div className="products-dots">
-        {[...Array(totalPages)].map((_, index) => (
-          <button
-            key={index}
-            className={`dot ${
-              currentPage === index ? "active-dot" : ""
-            }`}
-            onClick={() => setCurrentPage(index)}
-          />
-        ))}
+                <button className="product-price">
+                  {product.price}
+                </button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
