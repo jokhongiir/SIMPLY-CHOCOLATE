3987dbc92ba18ffa481cee14f7ff501cfc7a5d7a
raxmatjonoww <raxmatjonovjahongir5@gmail.com>
2026-06-18 21:29:08 +0500

    Simply Chocolate

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -20,16 +20,13 @@ const Subscribe = () => {
           </h2>
 
           <p className="subscribe-text">
-            From the velvety texture to the rich and complex flavor,
-            our New Chocolate is a true indulgence that will leave
-            you craving more. Made with only the finest ingredients
-            and handcrafted with the utmost care, each piece is a
-            work of art that is sure to please.
+            From the velvety texture to the rich and complex flavor, our New
+            Chocolate is a true indulgence that will leave you craving more.
+            Made with only the finest ingredients and handcrafted with the
+            utmost care, each piece is a work of art that is sure to please.
           </p>
 
-          <button className="subscribe-btn">
-            Subscribe
-          </button>
+          <button className="subscribe-btn">Subscribe</button>
         </div>
       </div>
     </section>
